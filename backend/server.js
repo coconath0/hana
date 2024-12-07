7f5a90e4e6b57ad422635ca8a55e34ad25d521ba
coconath0 <naingol10@gmail.com>
2024-12-06 22:46:05 -0600

    uwu

--- backend/server.js
+++ backend/server.js
@@ -110,7 +110,7 @@ app.put('/flowers/:id/restock', async (req, res) => {
     res.send('Flower restocked');
 });
 
-app.use(express.static(path.join(__dirname, '..', 'src', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
